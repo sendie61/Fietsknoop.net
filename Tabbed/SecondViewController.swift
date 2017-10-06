e2c775647ab1a8d7ada5177b51be18441fb1bb41
sendie61 <sendie61@hotmail.com>
2017-10-06 22:23:14 +0200

    First commit

--- Tabbed/SecondViewController.swift
+++ Tabbed/SecondViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +20,6 @@ class SecondViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
