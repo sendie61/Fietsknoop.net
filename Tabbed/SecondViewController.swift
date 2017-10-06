67a6fa47443c5d72b3575a3b02202a4d1e72be67
sendie61 <sendie61@hotmail.com>
2017-10-07 00:01:22 +0200

    Not fully stable yet...

--- Tabbed/SecondViewController.swift
+++ Tabbed/SecondViewController.swift
@@ -8,9 +8,44 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    var list = ["21", "34", "45", "23", "11", "34", "68"]
     
+    @IBOutlet weak var myTableView: UITableView!
+
+    @IBOutlet weak var input: UITextField!
+  
+    @IBAction func addItem(_ sender: Any)
+    {
+        if input.text != ""
+        {
+            list.append(input.text!)
+            myTableView.reloadData()
+        }
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return list.count
+    }
+
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell" )
+        cell.textLabel?.text = list[indexPath.row]
+        return (cell)
+    }
+
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath)
+    {
+        if editingStyle == UITableViewCellEditingStyle.delete
+        {
+            list.remove(at: indexPath.row)
+            myTableView.reloadData()
+        }      
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
